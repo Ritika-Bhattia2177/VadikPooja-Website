ac178b07ad5165e0121f18f8ebea11a087e17c86
sonukathat <skkhankathat93@gmail.com>
2026-03-20 20:22:35 +0530

    update pandit api in backend

--- backend/routes/panditRoutes.js
+++ backend/routes/panditRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { getPandits, createPandit } from '../controllers/panditsController.js';
+import { getPandits, createPandit, updatePandit, deletePandit } from '../controllers/panditsController.js';
 
 const router = Router();
 
 router.get('/', getPandits);
 router.post('/', createPandit);
+router.put('/:id', updatePandit);
+router.delete('/:id', deletePandit);
 
 export default router;
